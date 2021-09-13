c86bb252dcb95405213e4508562550e8ef26f63c
Aaqib <muhammad.aaqib@koderlabs.com>
2021-09-14 00:04:16 +0500

    Splash screen added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   useColorScheme,
 } from 'react-native';
@@ -12,7 +12,18 @@ import { persistor, store } from "./src/redux/store"
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
 import { primaryColor } from './src/theme/styles';
+import RNBootSplash from "react-native-bootsplash";
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true });
+      console.log("Bootsplash has been hidden successfully");
+    });
+  }, []);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -23,7 +34,7 @@ const App = () => {
     <>
       <Provider store={store} >
         <PersistGate loading={null} persistor={persistor}>
-        <StatusBarComponent barStyle={'light-content'} backgroundColor={primaryColor} />
+          <StatusBarComponent barStyle={'light-content'} backgroundColor={primaryColor} />
           <TabNavigator />
         </PersistGate>
       </Provider>
